d63d94d5b304abdf276a1ec86191b3daa9ca8abc
CHENG-TING CHEN <abfa762466@gmail.com>
2025-04-28 14:24:16 +0800

    fix: tab instead of 4 spaces

--- lua/ctchen/core/options.lua
+++ lua/ctchen/core/options.lua
@@ -8,7 +8,7 @@ opt.number = true
 -- tabs & indentation
 opt.tabstop = 4 -- 4 spaces for tabs (prettier default)
 opt.shiftwidth = 4 -- 4 spaces for indent width
-opt.expandtab = true -- expand tab to spaces
+opt.expandtab = false -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
 opt.wrap = false
