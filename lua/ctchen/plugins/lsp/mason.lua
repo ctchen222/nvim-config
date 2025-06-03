a2da2ec58e49d0e1a846c9b2a3de823f4ad98c0e
CHENG-TING CHEN <abfa762466@gmail.com>
2025-06-03 22:20:03 +0800

    feat: temp lsp & mason config

--- lua/ctchen/plugins/lsp/mason.lua
+++ lua/ctchen/plugins/lsp/mason.lua
@@ -1,81 +1,150 @@
+-- return {
+--   {
+--     "williamboman/mason.nvim",
+--     version = "1.10.0",
+--     lazy = false,
+--     config = function()
+--       require("mason").setup({
+--         ui = {
+--           icons = {
+--             package_installed = "✓",
+--             package_pending = "➜",
+--             package_uninstalled = "✗",
+--           },
+--         },
+--         log_level = vim.log.levels.DEBUG, -- 啟用調試日誌
+--       })
+--     end,
+--   },
+--   {
+--     "williamboman/mason-lspconfig.nvim", -- 修正倉庫名稱
+--     version = "1.31.0",
+--     lazy = false,
+--     dependencies = { "williamboman/mason.nvim" },
+--     config = function()
+--       require("mason-lspconfig").setup({
+--         ensure_installed = {
+--           "html",
+--           "cssls",
+--           "tailwindcss",
+--           "lua_ls",
+--           "graphql",
+--           "emmet_ls",
+--           "pyright",
+--           "eslint",
+--           "gopls",
+--         },
+--         automatic_installation = true,
+--       })
+--     end,
+--   },
+--   {
+--     "jay-babu/mason-null-ls.nvim",
+--     lazy = false,
+--     dependencies = { "williamboman/mason.nvim", "nvimtools/none-ls.nvim" },
+--     config = function()
+--       require("mason-null-ls").setup({
+--         ensure_installed = {
+--           "prettier", -- js/ts formatter
+--           "stylua", -- lua formatter
+--           "isort", -- python formatter
+--           "black", -- python formatter
+--           "pylint", -- python linter
+--           "eslint", -- js/ts linter
+--         },
+--         automatic_installation = false,
+--       })
+--     end,
+--   },
+--   {
+--     "WhoIsSethDaniel/mason-tool-installer.nvim",
+--     lazy = false,
+--     dependencies = { "williamboman/mason.nvim" },
+--     config = function()
+--       require("mason-tool-installer").setup({
+--         ensure_installed = {
+--           "stylua", -- lua formatter
+--           "isort", -- python formatter
+--           "black", -- python formatter
+--           "pylint", -- python linter
+--           "eslint", -- js/ts linter
+--         },
+--       })
+--     end,
+--   },
+--   config = function()
+--     -- import plugins safely
+--     local mason = require("mason")
+--     local mason_lspconfig = require("mason-lspconfig")
+--     local mason_null_ls = require("mason-null-ls")
+--     local mason_tool_installer = require("mason-tool-installer")
+--
+--     -- enable mason and configure icons
+--     mason.setup({
+--       ui = {
+--         icons = {
+--           package_installed = "✓",
+--           package_pending = "➜",
+--           package_uninstalled = "✗",
+--         },
+--       },
+--     })
+--
+--     -- configure lsp servers to be installed
+--     mason_lspconfig.setup({
+--       ensure_installed = {
+--         "html",
+--         "cssls",
+--         "tailwindcss",
+--         "lua_ls",
+--         "graphql",
+--         "emmet_ls",
+--         "pyright",
+--         "eslint",
+--         "gopls",
+--       },
+--     })
+--
+--     -- configure formatters/linters to be installed
+--     mason_tool_installer.setup({
+--       ensure_installed = {
+--         "stylua", -- lua formatter
+--         "isort", -- python formatter
+--         "black", -- python formatter
+--         "pylint", -- python linter
+--         "eslint", -- js/ts linter
+--       },
+--     })
+--
+--     -- configure null-ls sources to be installed
+--     mason_null_ls.setup({
+--       ensure_installed = {
+--         "prettier", -- js/ts formatter
+--         "stylua", -- lua formatter
+--         "isort", -- python formatter
+--         "black", -- python formatter
+--         "pylint", -- python linter
+--         "eslint", -- js/ts linter
+--       },
+--       -- automatically setup null-ls with the installed sources
+--       automatic_installation = false,
+--     })
+--   end,
+-- }
+--
 return {
-  {
-    "williamboman/mason.nvim",
-    version = "1.10.0",
-    lazy = false,
-    config = function()
-      require("mason").setup({
-        ui = {
-          icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗",
-          },
-        },
-        log_level = vim.log.levels.DEBUG, -- 啟用調試日誌
-      })
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim", -- 修正倉庫名稱
-    version = "1.31.0",
-    lazy = false,
-    dependencies = { "williamboman/mason.nvim" },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "html",
-          "cssls",
-          "tailwindcss",
-          "lua_ls",
-          "graphql",
-          "emmet_ls",
-          "pyright",
-          "eslint",
-        },
-        automatic_installation = true,
-      })
-    end,
-  },
-  {
-    "jay-babu/mason-null-ls.nvim",
-    lazy = false,
-    dependencies = { "williamboman/mason.nvim", "nvimtools/none-ls.nvim" },
-    config = function()
-      require("mason-null-ls").setup({
-        ensure_installed = {
-          "prettier", -- js/ts formatter
-          "stylua", -- lua formatter
-          "isort", -- python formatter
-          "black", -- python formatter
-          "pylint", -- python linter
-          "eslint", -- js/ts linter
-        },
-        automatic_installation = false,
-      })
-    end,
-  },
-  {
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
-    lazy = false,
-    dependencies = { "williamboman/mason.nvim" },
-    config = function()
-      require("mason-tool-installer").setup({
-        ensure_installed = {
-          "stylua", -- lua formatter
-          "isort", -- python formatter
-          "black", -- python formatter
-          "pylint", -- python linter
-          "eslint", -- js/ts linter
-        },
-      })
-    end,
   },
   config = function()
-    -- import plugins safely
+    -- import mason
     local mason = require("mason")
+
+    -- import mason-lspconfig
     local mason_lspconfig = require("mason-lspconfig")
-    local mason_null_ls = require("mason-null-ls")
+
     local mason_tool_installer = require("mason-tool-installer")
 
     -- enable mason and configure icons
@@ -89,43 +158,31 @@ return {
       },
     })
 
-    -- configure lsp servers to be installed
     mason_lspconfig.setup({
+      -- list of servers for mason to install
       ensure_installed = {
+        "tsserver",
         "html",
         "cssls",
         "tailwindcss",
+        "svelte",
         "lua_ls",
         "graphql",
         "emmet_ls",
+        "prismals",
         "pyright",
-        "eslint",
       },
     })
 
-    -- configure formatters/linters to be installed
     mason_tool_installer.setup({
       ensure_installed = {
+        "prettier", -- prettier formatter
         "stylua", -- lua formatter
         "isort", -- python formatter
         "black", -- python formatter
-        "pylint", -- python linter
-        "eslint", -- js/ts linter
+        "pylint",
+        "eslint_d",
       },
-    })
-
-    -- configure null-ls sources to be installed
-    mason_null_ls.setup({
-      ensure_installed = {
-        "prettier", -- js/ts formatter
-        "stylua", -- lua formatter
-        "isort", -- python formatter
-        "black", -- python formatter
-        "pylint", -- python linter
-        "eslint", -- js/ts linter
-      },
-      -- automatically setup null-ls with the installed sources
-      automatic_installation = false,
     })
   end,
 }
