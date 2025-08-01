f3d2ee560ff4f7b95988cfe5bd905f828eadc477
CHENG-TING CHEN <abfa762466@gmail.com>
2025-08-01 17:04:11 +0800

    feat: trouble.nvim toggle

--- lua/ctchen/plugins/trouble.lua
+++ lua/ctchen/plugins/trouble.lua
@@ -9,7 +9,11 @@ return {
     { "<leader>xw", "<cmd>Trouble diagnostics toggle<CR>", desc = "Open trouble workspace diagnostics" },
     { "<leader>xd", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", desc = "Open trouble document diagnostics" },
     { "<leader>xq", "<cmd>Trouble quickfix toggle<CR>", desc = "Open trouble quickfix list" },
-    { "<leader>xl", "<cmd>Trouble loclist toggle<CR>", desc = "Open trouble location list" },
     { "<leader>xt", "<cmd>Trouble todo toggle<CR>", desc = "Open todos in trouble" },
+    {
+      "<leader>xl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
   },
 }
