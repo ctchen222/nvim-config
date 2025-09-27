98e2276138545ecd19d4aeaf107c495645203ab2
CHENG-TING CHEN <abfa762466@gmail.com>
2025-09-27 23:40:47 +0800

    feat: remove autofold for copilotChat

--- lua/ctchen/plugins/copilot.lua
+++ lua/ctchen/plugins/copilot.lua
@@ -16,7 +16,6 @@ return {
         tool = "🔧 Tool ",
       },
       separator = "━━",
-      auto_fold = false, -- Automatically folds non-assistant messages
       window = {
         layout = "vertical", -- 'vertical', 'horizontal', 'float'
         width = 0.5, -- 50% of screen width
