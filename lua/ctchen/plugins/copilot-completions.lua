a52000cab4430342f7b4ada18452542d03249696
CHENG-TING CHEN <abfa762466@gmail.com>
2025-12-14 19:06:00 +0800

    feat(copilot): add custom <C-l> keymap and disable default tab mapping

--- lua/ctchen/plugins/copilot-completions.lua
+++ lua/ctchen/plugins/copilot-completions.lua
@@ -2,4 +2,8 @@ return {
   "github/copilot.vim",
   event = "InsertEnter",
   -- lazy = "VeryLazy",
+  config = function()
+    vim.g.copilot_no_tab_map = true
+    vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', { expr = true, silent = true, noremap = true })
+  end,
 }
