11e2d5912fb61d3c3468f4434345d7d46d33beaa
CTCHEN <abfa762466@gmail.com>
2026-01-17 23:41:16 +0800

    chore: disable lazydocker plugin configuration

--- lua/ctchen/plugins/lazydocker.lua
+++ lua/ctchen/plugins/lazydocker.lua
@@ -1,23 +1,23 @@
 return {
-  "crnvl96/lazydocker.nvim",
-  cmd = {
-    "LazyDocker",
-    "LazyDockerConfig",
-  },
-  dependencies = {
-    "MunifTanjim/nui.nvim", -- 必須依賴
-  },
-  keys = {
-    { "<leader>ld", "<cmd>LazyDocker<cr>", desc = "Open LazyDocker" },
-  },
-  config = function()
-    require("lazydocker").setup({
-      terminal = {
-        direction = "float", -- 終端顯示方式: "float", "horizontal", 或 "vertical"
-        float_opts = {
-          border = "rounded", -- 浮動窗口邊框樣式
-        },
-      },
-    })
-  end,
+  -- "crnvl96/lazydocker.nvim",
+  -- cmd = {
+  --   "LazyDocker",
+  --   "LazyDockerConfig",
+  -- },
+  -- dependencies = {
+  --   "MunifTanjim/nui.nvim", -- 必須依賴
+  -- },
+  -- keys = {
+  --   { "<leader>ld", "<cmd>LazyDocker<cr>", desc = "Open LazyDocker" },
+  -- },
+  -- config = function()
+  --   require("lazydocker").setup({
+  --     terminal = {
+  --       direction = "float", -- 終端顯示方式: "float", "horizontal", 或 "vertical"
+  --       float_opts = {
+  --         border = "rounded", -- 浮動窗口邊框樣式
+  --       },
+  --     },
+  --   })
+  -- end,
 }
