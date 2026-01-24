39ad1ec52b04a9aff35eb06dfae521cd0fbed590
CTCHEN <abfa762466@gmail.com>
2026-01-24 18:10:49 +0800

    feat(plugins): enhance dadbod config and add mongodb support

--- lua/ctchen/plugins/dadbod.lua
+++ lua/ctchen/plugins/dadbod.lua
@@ -3,7 +3,7 @@ return {
     "kristijanhusak/vim-dadbod-ui",
     dependencies = {
       { "tpope/vim-dadbod", lazy = true },
-      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql", "mongodb" }, lazy = true },
     },
     cmd = {
       "DBUI",
@@ -11,24 +11,25 @@ return {
       "DBUIAddConnection",
       "DBUIFindBuffer",
     },
+    keys = {
+      { "<leader>Dt", "<cmd>DBUIToggle<cr>", desc = "Toggle DBUI" },
+      { "<leader>Da", "<cmd>DBUIAddConnection<cr>", desc = "Add DB Connection" },
+      { "<leader>Df", "<cmd>DBUIFindBuffer<cr>", desc = "Find DB Buffer" },
+      { "<leader>Dl", "<cmd>DBUILastQueryInfo<cr>", desc = "Last Query Info" },
+    },
     init = function()
-      -- Your DBUI configuration
       vim.g.db_ui_use_nerd_fonts = 1
+      vim.g.db_ui_save_location = vim.fn.expand("~/.local/share/db_ui")
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "sql", "mysql", "plsql", "mongodb" },
+        callback = function()
+          local bufnr = vim.api.nvim_get_current_buf()
+          vim.keymap.set("n", "<leader>De", "<cmd>%DB<cr>", { buffer = bufnr, desc = "Execute All" })
+          vim.keymap.set("n", "<leader>Dr", "<cmd>.DB<cr>", { buffer = bufnr, desc = "Execute Line" })
+          vim.keymap.set("v", "<leader>De", ":DB<cr>", { buffer = bufnr, desc = "Execute Selection" })
+        end,
+      })
     end,
   },
-  -- { -- optional saghen/blink.cmp completion source
-  --   "saghen/blink.cmp",
-  --   opts = {
-  --     sources = {
-  --       default = { "lsp", "path", "snippets", "buffer" },
-  --       per_filetype = {
-  --         sql = { "snippets", "dadbod", "buffer" },
-  --       },
-  --       -- add vim-dadbod-completion to your completion providers
-  --       providers = {
-  --         dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
-  --       },
-  --     },
-  --   },
-  -- },
 }
