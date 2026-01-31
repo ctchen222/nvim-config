ea7299b04056f93c0cda1b47ebce77685601542d
CTCHEN <abfa762466@gmail.com>
2026-01-31 22:01:58 +0800

    chore(plugins): disable markdown-preview in favor of markview

--- lua/ctchen/plugins/markdown.lua
+++ lua/ctchen/plugins/markdown.lua
@@ -1,15 +1,17 @@
-return {
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && yarn install",
-  config = function()
-    vim.g.mkdp_filetypes = { "markdown" }
-
-    local keymap = vim.keymap
-
-    keymap.set("n", "<leader>mt", "<cmd>MarkdownPreviewToggle<cr>", { desc = "Toggle markdown preview" })
-    keymap.set("n", "<leader>mo", "<cmd>MarkdownPreview<cr>", { desc = "Open markdown preview" })
-    keymap.set("n", "<leader>mc", "<cmd>MarkdownPreviewStop<cr>", { desc = "Close markdown preview" })
-  end,
-  ft = { "markdown" },
-}
+-- Disabled: using markview.nvim instead (lighter weight, native rendering)
+-- return {
+--   "iamcco/markdown-preview.nvim",
+--   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+--   build = "cd app && yarn install",
+--   config = function()
+--     vim.g.mkdp_filetypes = { "markdown" }
+--
+--     local keymap = vim.keymap
+--
+--     keymap.set("n", "<leader>mt", "<cmd>MarkdownPreviewToggle<cr>", { desc = "Toggle markdown preview" })
+--     keymap.set("n", "<leader>mo", "<cmd>MarkdownPreview<cr>", { desc = "Open markdown preview" })
+--     keymap.set("n", "<leader>mc", "<cmd>MarkdownPreviewStop<cr>", { desc = "Close markdown preview" })
+--   end,
+--   ft = { "markdown" },
+-- }
+return {}
