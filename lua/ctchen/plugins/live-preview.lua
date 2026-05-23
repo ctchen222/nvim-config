0caf4bdb709182465a58b6e47d0f6a11ef4f8176
CTCHEN <abfa762466@gmail.com>
2026-05-23 14:34:56 +0800

    feat(plugins): add keymaps for live-preview actions

--- lua/ctchen/plugins/live-preview.lua
+++ lua/ctchen/plugins/live-preview.lua
@@ -3,4 +3,23 @@ return {
   dependencies = {
     "nvim-telescope/telescope.nvim",
   },
+  keys = {
+    {
+      "<leader>lt",
+      function()
+        local livepreview = require("livepreview")
+
+        if livepreview.is_running() then
+          vim.cmd("LivePreview close")
+        else
+          vim.cmd("LivePreview start")
+        end
+      end,
+      desc = "LivePreview: Toggle",
+    },
+    { "<leader>ls", "<cmd>LivePreview start<cr>", desc = "LivePreview: Start" },
+    { "<leader>lc", "<cmd>LivePreview close<cr>", desc = "LivePreview: Close" },
+    { "<leader>lp", "<cmd>LivePreview pick<cr>", desc = "LivePreview: Pick file" },
+    { "<leader>lh", "<cmd>LivePreview help<cr>", desc = "LivePreview: Help" },
+  },
 }
