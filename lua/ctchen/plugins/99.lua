3b36400cec29d6e4503ffd9842de1da908aac3d3
CTCHEN <abfa762466@gmail.com>
2026-07-09 17:36:50 +0800

    fix(99): comment out AI keymaps

--- lua/ctchen/plugins/99.lua
+++ lua/ctchen/plugins/99.lua
@@ -8,8 +8,8 @@ return {
       md_files = { "AGENT.md" },
     })
 
-    vim.keymap.set("v", "<leader>9v", _99.visual, { desc = "99: AI Replace Selection" })
-    vim.keymap.set("n", "<leader>9s", _99.search, { desc = "99: AI Search" })
-    vim.keymap.set("n", "<leader>9x", _99.stop_all_requests, { desc = "99: Stop All Requests" })
+    -- vim.keymap.set("v", "<leader>9v", _99.visual, { desc = "99: AI Replace Selection" })
+    -- vim.keymap.set("n", "<leader>9s", _99.search, { desc = "99: AI Search" })
+    -- vim.keymap.set("n", "<leader>9x", _99.stop_all_requests, { desc = "99: Stop All Requests" })
   end,
 }
